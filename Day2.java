2b77a23ccd5756f371f4fef012315d158bcf13e8
Fatmawati05 <fw841351@gmail.com>
2025-09-09 17:39:02 +0800

    penggunaan println(),print(),dan printf()

---
+++ Day2.java
@@ -0,0 +1,12 @@
+package Fatmawati;
+
+public class Day2 {
+  public static void main(String[] args) {
+    System.out.println("Baris ke bawah");
+
+    System.out.print("Baris lurus atau ke samping");
+
+    String nama = "Fatmawati";
+    System.out.printf("Nama : %s" , nama);
+  }
+}
