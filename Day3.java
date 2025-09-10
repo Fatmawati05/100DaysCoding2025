df91b4063a7293830a4b4451cb7e74d22da0788a
Fatmawati05 <fw841351@gmail.com>
2025-09-10 19:21:52 +0800

    Memahami dan menggunakan komentar di java

--- Day3.java
+++ Day3.java
@@ -4,20 +4,22 @@ public class Day3 {
     public static void main(String[] args) {
         
         // Komentar satu baris
-        System.out.println("Halo, Fatmawati"); // Bisa juga di ujung baris
+        // Bisa juga di ujung baris
+        System.out.println("Halo, Fatmawati"); // menampilkan Halo, Fatmawati di consule 
 
         /* Komentar banyak baris
            Dipakai kalau penjelasannya agak panjang
         */
-        int a = 10;
-        int b = 10;
+        
+        int a = 10; 
+        int b = 10; 
         int hasil = a + b; // Menjumlahkan a dan b
-        System.out.println("Hasil: " + hasil);
+        
+        System.out.println("Hasil: " + hasil); // menampilkan hasil 
 
         /**
          * Komentar dokumentasi (Javadoc)
          * Biasanya ditulis untuk class atau method
          */
-        System.out.println("Belajar komentar di Java itu mudah!");
     }
 }
