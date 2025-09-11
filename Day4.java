46973e33b264f2392280d606a9b3b89dc62f5163
Fatmawati05 <fw841351@gmail.com>
2025-09-11 18:36:42 +0800

    Penggunaan Escape Sequence (\n, \t, dan lain-lain).

---
+++ Day4.java
@@ -0,0 +1,32 @@
+package Fatmawati;
+public class Day4 {
+    public static void main(String[] args) {
+                // 1. \n = baris baru
+                System.out.println("Halo\nDunia");
+
+                // 2. \t = tab
+                System.out.println("Nama:\tFatmawati");
+                System.out.println("Umur:\t20");
+
+                // 3. \\ = backslash
+                System.out.println("Lokasi: C:\\Users\\Fatmawati");
+
+                // 4. \" = tanda kutip ganda
+                System.out.println("Dia berkata: \"Belajar Java itu seru!\"");
+
+                // 5. \' = tanda kutip tunggal
+                System.out.println("Karakter ini: \'A\'");
+
+                // 6. \r = carriage return (hasil bergantung OS/terminal)
+                System.out.println("Halo\rDunia");
+
+                // 7. \b = backspace
+                System.out.println("Helloo\b!");
+
+                // 8. \f = form feed (jarang dipakai, efeknya berbeda-beda)
+                System.out.println("Ini sebelum\fIni sesudah ");
+
+                //9. \u2665 = Unicode atau simbol (contoh simbol hati ♥)
+                System.out.println("Simbol hati: \u2665");
+    }
+}
