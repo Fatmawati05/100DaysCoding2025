b76f1cea9b858e2fcf5c80067f26340e518bfd95
Fatmawati05 <fw841351@gmail.com>
2025-09-12 19:17:22 +0800

    Tipe Data Numerik Bilangan Bulat ( byte, short, int, long )

---
+++ Day5.java
@@ -0,0 +1,16 @@
+package Fatmawati;
+
+public class Day5 {
+    public static void main(String[] args) {
+    
+        byte jumlahBuku = 120;
+        short jarakKota = 15000;
+        int hargaLaptop = 13500000;
+        long nomorSeri = 987654321012345678L;
+
+        System.out.println("jumlah buku: " + jumlahBuku);
+        System.out.println("jarak antar kota (meter): " + jarakKota);
+        System.out.println("harga laptop: " + hargaLaptop);
+        System.out.println("nomor seri barang: " + nomorSeri);
+    }
+}
