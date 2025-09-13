a752b517a50ca2be3a17ba2ee8752e9b22271684
Fatmawati05 <fw841351@gmail.com>
2025-09-13 20:30:09 +0800

    Tipe data pecahan (double, float)

---
+++ Day6.java
@@ -0,0 +1,12 @@
+package Fatmawati;
+
+public class Day6 {
+  public static void main(String[] args) {
+    
+    float angka1 = 3.3455f; // float bilangan desimal harus pakai huruf 'f' di akhir
+    System.out.println("Angka1 : " + angka1);
+    
+    double angka2 = 3.24; // double untuk bilangan desimal (lebih presisi dibanding float)
+    System.out.println("Angka2 : " + angka2);
+  }
+}
