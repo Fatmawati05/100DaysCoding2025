7d8262ae5e8056ba849d26bc1824d873b845be6d
Fatmawati05 <fw841351@gmail.com>
2025-09-15 10:05:37 +0800

    pengenalan tipe data string untuk teks

---
+++ Day8.java
@@ -0,0 +1,9 @@
+package Fatmawati;
+
+public class Day8 {
+    public static void main(String[] args) {
+        // String = tipe data buat nyimpen teks (huruf, angka, simbol) pakai tanda kutip ganda
+        String nama = "Fatmawati";
+        System.out.println(nama);
+    }
+}
