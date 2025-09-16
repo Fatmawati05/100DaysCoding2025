02159b857e2b917f1cc96ed6ca3e980203cd370d
Fatmawati05 <fw841351@gmail.com>
2025-09-16 20:00:00 +0800

    Deklarasi, Inisialisasi dan Update variabel

---
+++ Day9.java
@@ -0,0 +1,13 @@
+package Fatmawati;
+
+public class Day9 {
+  public static void main(String[] args) {
+    
+    int angka; // deklarasi variabel "angka" bertipe data int
+    angka = 10; // update variabel "angka" 
+    int a = 5999999; // inisialisasi variabel "a" sekaligus deklarasi 
+
+    System.out.println("angka : " + angka);
+    System.out.println("jumlah : " + a);
+  }
+}
