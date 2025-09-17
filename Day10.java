c034e60d57306766f8b490f3d82ba83e3ed209a6
Fatmawati05 <fw841351@gmail.com>
2025-09-17 19:45:05 +0800

    Constanta (Final Variabel)

--- Day10.java
+++ Day10.java
@@ -1,9 +1,9 @@
 public class Day10 {
     public static void main(String[] args) {
-        final String nama = "Fatmawati"; // constanta nama
-        final int umur = 20;             // constanta umur
+        final String NAMA = "Fatmawati"; // constanta nama
+        final int UMUR = 20;             // constanta umur
 
-        System.out.println("Nama : " + nama);
-        System.out.println("Umur : " + umur + " tahun");
+        System.out.println("Nama : " + NAMA);
+        System.out.println("Umur : " + UMUR + " tahun");
     }
 }
