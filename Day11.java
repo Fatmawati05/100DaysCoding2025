1b1393fefc17c63bfd179a584a41fea2860133ed
Fatmawati05 <fw841351@gmail.com>
2025-09-18 20:52:23 +0800

    Menerima input dari keyboard (pengenalan Scanner)

--- Day11.java
+++ Day11.java
@@ -1,13 +1,45 @@
-import java.util.Scanner; // import Scanner
+import java.util.Scanner;
 
 public class Day11 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // membuat objek Scanner
+        Scanner input = new Scanner(System.in);
 
-        System.out.print("Nama : "); 
-        String nama = sc.nextLine(); // membaca input berupa teks karna kita pake Stirng
+        // String → membaca teks (contoh: nama)
+        System.out.print("String : ");
+        String nama = input.nextLine(); 
 
-        System.out.print("Umur : "); // membaca input berupa angka karna kita pake integer
-        int umur = sc.nextInt();
+        // int → membaca angka bulat (contoh: umur)
+        System.out.print("int : ");
+        int umur = input.nextInt(); 
+
+        // double → membaca angka desimal besar (contoh: tinggi)
+        System.out.print("double : ");
+        double tinggi = input.nextDouble(); 
+
+        // float → membaca angka desimal kecil (contoh: berat)
+        System.out.print("float : ");
+        float berat = input.nextFloat(); 
+
+        // long → membaca angka bulat besar (contoh: nomor besar)
+        System.out.print("long : ");
+        long nomor = input.nextLong(); 
+
+        // byte → membaca angka kecil
+        System.out.print("byte : ");
+        byte nilaiByte = input.nextByte(); 
+
+        // short → membaca angka sedang
+        System.out.print("short : ");
+        short nilaiShort = input.nextShort(); 
+
+        // boolean → membaca logika (true / false)
+        System.out.print("boolean : ");
+        boolean mahasiswa = input.nextBoolean(); 
+
+        // char → membaca 1 huruf (contoh: L atau P)
+        System.out.print("char : ");
+        char gender = input.next().charAt(0); 
+
+        input.close();
     }
 }
