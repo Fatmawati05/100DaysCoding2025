74155d57642304e45767488c027fbbf043e0cdd7
Fatmawati05 <fw841351@gmail.com>
2025-09-19 13:57:15 +0800

    Latihan : Membuat Program Biodata Sederhana

--- Day12.java
+++ Day12.java
@@ -3,33 +3,33 @@ import java.util.Scanner;
 public class Day12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-                // Input data
-                System.out.print("Nama : ");
-                String nama = sc.nextLine();
+        // Input data
+        System.out.print("Nama : ");
+        String nama = sc.nextLine();
                 
-                System.out.print("Umur : ");
-                int umur = sc.nextInt();
-                sc.nextLine(); // membersihkan enter
+        System.out.print("Umur : ");
+        int umur = sc.nextInt();
+        sc.nextLine(); // membersihkan enter
 
-                System.out.print("Alamat : ");
-                String alamat = sc.nextLine();
+        System.out.print("Alamat : ");
+        String alamat = sc.nextLine();
 
-                System.out.print("Hobi : ");
-                String hobi = sc.nextLine();
+        System.out.print("Hobi : ");
+        String hobi = sc.nextLine();
 
-                System.out.print("Tinggi Badan (cm) : ");
-                double tinggiB = sc.nextDouble();
+        System.out.print("Tinggi Badan (cm) : ");
+        double tinggiB = sc.nextDouble();
 
-                System.out.print("Berat Badan (kg) : ");
-                double beratB = sc.nextDouble();
+        System.out.print("Berat Badan (kg) : ");
+        double beratB = sc.nextDouble();
 
-                // Output data
-                System.out.println("BIODATA SAYA");
-                System.out.println("Nama         : " + nama);
-                System.out.println("Umur         : " + umur + " tahun");
-                System.out.println("Alamat       : " + alamat);
-                System.out.println("Hobi         : " + hobi);
-                System.out.println("Tinggi Badan : " + tinggiB + " cm");
-                System.out.println("Berat Badan  : " + beratB + " kg");
-            }
-                  }
+        // Output data
+        System.out.println("BIODATA SAYA");
+        System.out.println("Nama         : " + nama);
+        System.out.println("Umur         : " + umur + " tahun");
+        System.out.println("Alamat       : " + alamat);
+        System.out.println("Hobi         : " + hobi);
+        System.out.println("Tinggi Badan : " + tinggiB + " cm");
+        System.out.println("Berat Badan  : " + beratB + " kg");
+    }
+}
