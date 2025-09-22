f4525d47d1edc6786c516866144525255637dc37
Fatmawati05 <fw841351@gmail.com>
2025-09-22 20:00:16 +0800

    Operator aritmatika penjumlahan (+) dan pengurangan (-)

---
+++ Day15.java
@@ -0,0 +1,16 @@
+public class Day15 {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 16;
+
+        // operator aritmatika penjumlahan +
+        int hasilJumlah = a + b;
+        // operator aritmatika pengurangan -
+        int hasilKurang = a - b;
+
+        // ouputnya
+        System.out.println("Hasil penjumlahan : " + hasilJumlah); // hasilnya 36
+        System.out.println("Hasil pengurangan : " + hasilKurang); // hasilnya 4
+    }
+}
