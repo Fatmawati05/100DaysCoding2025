38be09867d3eb0ee5d7deb50d78fc5a5b4ff03d6
Fatmawati05 <fw841351@gmail.com>
2025-09-24 20:01:13 +0800

    Operator aritmatika modulus atau sisa bagi (%)

---
+++ Day17.java
@@ -0,0 +1,12 @@
+public class Day17 {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 3;
+        // operator aritmatika modulus atau sisa bagi %
+        int hasilSisa = a % b;
+        // ouputnya
+        System.out.println("Hasil sisa bagi : " + hasilSisa); // hasilnya 2
+      
+    }
+}
