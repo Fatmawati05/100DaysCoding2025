c4e0a2ed1d2fd798d8eac1627a84083db0e3896f
Fatmawati05 <fw841351@gmail.com>
2025-09-29 18:56:47 +0800

    Latihan: Program Luas Persegi

---
+++ Day22.java
@@ -0,0 +1,13 @@
+public class Day22 {
+    public static void main(String[] args) {
+        // mentukan sisi secara langsung
+        int sisi = 8;
+
+        // menghitung luas
+        int luas = sisi * sisi;
+
+        // menampilkan hasil
+        System.out.println("Sisi persegi = " + sisi);
+        System.out.println("Luas persegi = " + luas);
+    }
+}
