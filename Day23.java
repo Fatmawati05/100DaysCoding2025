95fa4101c81d1ba14daf24b02935d061ed8f2218
Fatmawati05 <fw841351@gmail.com>
2025-09-30 20:51:17 +0800

    Latihan: Program Luas Persegi Panjang

---
+++ Day23.java
@@ -0,0 +1,12 @@
+public class Day23 {
+    public static void main(String[] args) {
+        // nilai panjang dan lebar
+        int panjang = 10;
+        int lebar = 4;
+
+        // hitung luas
+        int luas = panjang * lebar;
+        // output
+        System.out.println("Luas Persegi Panjang : " + luas); // 40
+    }
+}
