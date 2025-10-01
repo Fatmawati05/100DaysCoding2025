bd3224df4dc51caab6b1bc94931c86353fc9552b
Fatmawati05 <fw841351@gmail.com>
2025-10-01 19:15:22 +0800

    Latihan: Program Luas Lingkaran.

---
+++ Day24.java
@@ -0,0 +1,11 @@
+public class Day24 {
+    public static void main(String[] args) {
+        double jari_jari = 10;
+        double nilaiPhi = 3.14;
+
+        double luas = nilaiPhi * jari_jari * jari_jari;
+
+        System.out.println("Nilai Jari-jari :  " + jari_jari); // hasilnya 10.0
+        System.out.println("Nilai Luas Lingkaran : " + luas); // hasilnya 314.0
+    }
+}
