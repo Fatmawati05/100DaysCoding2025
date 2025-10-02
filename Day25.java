0305ec775df983ee71aa8e21d1e36195be20ca0f
Fatmawati05 <fw841351@gmail.com>
2025-10-02 20:03:58 +0800

    Operator Increment dan Decrement (++, --)

---
+++ Day25.java
@@ -0,0 +1,13 @@
+public class Day25 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("i = " + i); // hasilnya 1 sampai 10
+        }
+        System.out.println(" ");
+
+        for (int j = 10; j >= 1; j--) {
+            System.out.println("j = " + j); // hasilnya 10 sampai 1
+
+        }
+    }
+}
