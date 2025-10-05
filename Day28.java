a1636ab81cb3e0c2b192da2e6dac7ed4aa5332dc
Fatmawati05 <fw841351@gmail.com>
2025-10-05 20:03:03 +0800

    Operator Perbandingan == dan !=

---
+++ Day28.java
@@ -0,0 +1,9 @@
+public class Day28 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+
+        System.out.println(a == b); // hasilnya true
+        System.out.println(a != b); // hasilnya false
+    }
+}
