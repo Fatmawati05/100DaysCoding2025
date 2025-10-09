2dbb6245d76f64b34a6b0e000ad52e442d16033b
Fatmawati05 <fw841351@gmail.com>
2025-10-09 21:34:30 +0800

    Operator Logika OR ( || ).

---
+++ Day32.java
@@ -0,0 +1,9 @@
+public class Day32 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a > 8 || b > 8);  // true, karena a > 8 benar
+        System.out.println(a < 3 || b < 3);  // false, karena keduanya salah
+    }
+}
