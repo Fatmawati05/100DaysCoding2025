2250a01bd827ddd3dfbfe0b55a70b23161caa09b
Fatmawati05 <fw841351@gmail.com>
2025-10-29 18:53:42 +0800

    Perulangan while

---
+++ Day52.java
@@ -0,0 +1,10 @@
+public class Day52 {
+    public static void main(String[] args) {
+        int angka = 1;
+
+        while (angka <= 5) {
+            System.out.println("perulangan : " + angka);
+            angka++;
+        }
+    }
+}
