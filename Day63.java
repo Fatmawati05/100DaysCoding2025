93e6d3548618a612c3268521285eda1a2c7a6423
Fatmawati05 <fw841351@gmail.com>
2025-11-09 20:59:39 +0800

    Latihan: Mengalikan 1-N

---
+++ Day63.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Day63 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan N : ");
+        int n = sc.nextInt();
+
+        int total = 1;
+        for (int i = 1; i <= n; i++) {
+            total *= i;
+        }
+        System.out.println("perkalian 1 sampai " + n + " adalah : " + total);
+    }
+}
