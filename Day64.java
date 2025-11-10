fb2ca8e05ed1eaad4ccb9db568ee818163ae90e2
Fatmawati05 <fw841351@gmail.com>
2025-11-10 21:03:23 +0800

    Latihan: Mencetak hasil M pangkat N

---
+++ Day64.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Day64 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan M : ");
+        int m = sc.nextInt();
+        System.out.print("Masukkan N : ");
+        int n = sc.nextInt();
+
+        int hasil = 1;
+        for (int i = 0; i < n; i++) {
+            hasil *= m;
+        }
+        System.out.println("Hasil dari " + m + " pangkat " + n + " adalah = " + hasil);
+    }
+}
