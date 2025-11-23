12e4707097824381ffc95b5a3ec5b9cd2d9dc7a4
Fatmawati05 <fw841351@gmail.com>
2025-11-23 23:02:05 +0800

    Method String untuk Manipulasi (substring(), replace(), trim()).

---
+++ Day77.java
@@ -0,0 +1,19 @@
+public class Day77 {
+    public static void main(String[] args) {
+        String teks = "Fatmawati";
+
+        // substring()
+        String sub = teks.substring(2, 10);
+
+        // replace()
+        String ganti = teks.replace("Java", "Coding");
+
+        // trim()
+        String rapih = teks.trim();
+
+        System.out.println("Teks asli: \"" + teks + "\"");
+        System.out.println("Hasil substring: " + sub);
+        System.out.println("Hasil replace: " + ganti);
+        System.out.println("Hasil trim: " + rapih);
+    }
+}
