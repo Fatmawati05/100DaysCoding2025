f24fd77eb0a1d0d47d8bd26c389e4996a735cb2d
Fatmawati05 <fw841351@gmail.com>
2025-11-26 22:34:24 +0800

    Array 1D

---
+++ Day80.java
@@ -0,0 +1,9 @@
+public class Day80 {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println(angka[i]);
+        }
+    }
+}
