fa77a569b133b8d12580bcc983f84517aea5330b
Fatmawati05 <fw841351@gmail.com>
2025-11-28 22:01:01 +0800

    Latihan: Mengisi Elemen Array dan Menampilkan semua elemen Array dengan Perulangan

---
+++ Day82.java
@@ -0,0 +1,10 @@
+public class Day82 {
+    public static void main(String[] args) {
+      
+        int[] angka = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Elemen indeks " + i + ": " + angka[i]);
+        }
+    }
+}
