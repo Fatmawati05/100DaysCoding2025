d5093b3f8a316a903cfd843067fa16230d18bcd9
Fatmawati05 <fw841351@gmail.com>
2025-12-04 21:47:29 +0800

    Mengakses Elemen Array dengan Menggunakan For-each

---
+++ Day88.java
@@ -0,0 +1,8 @@
+public class Day88 {
+    public static void main(String[] args) {
+        int angka[] = {10, 20, 30, 40};
+        for (int nilai : angka){
+            System.out.println("Nilai : " + nilai);
+        }
+    }
+}
