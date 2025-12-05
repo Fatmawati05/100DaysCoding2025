2e3d9b9f63e054becb77af9a006f23abc9bcacfe
Fatmawati05 <fw841351@gmail.com>
2025-12-05 21:40:55 +0800

    Method Void

---
+++ Day89.java
@@ -0,0 +1,8 @@
+public class Day89 {
+    static void perkalian (int alas, int tinggi){
+        System.out.println(alas*tinggi/2);
+    }
+    public static void main(String[] args) {
+        perkalian(21, 7);
+    }
+}
