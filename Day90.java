03f559e689c75ee434c09dbee522191ea9c7918b
Fatmawati05 <fw841351@gmail.com>
2025-12-06 20:40:52 +0800

    Method Non-Void

---
+++ Day90.java
@@ -0,0 +1,11 @@
+public class Day90 {
+    public static int jumlahkan(int a, int b) {
+        return a + b;  
+    }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int hasil = jumlahkan(a, b);  
+        System.out.println("Hasil penjumlahan: " + hasil); 
+    }
+}
