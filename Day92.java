02172676e008045d5bab40f9343351ccd29ce0fc
Fatmawati05 <fw841351@gmail.com>
2025-12-08 18:49:24 +0800

    Keyword return

---
+++ Day92.java
@@ -0,0 +1,10 @@
+public class Day92 {
+
+    public static int hitung(int a, int b) {
+        return a + b; 
+    }
+    public static void main(String[] args) {
+        int hasil = hitung(4, 6);
+        System.out.println("Hasil: " + hasil);
+    }
+}
