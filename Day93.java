677e75ea35bed5052b8c19f0f31d7eb5f88a27b0
Fatmawati05 <fw841351@gmail.com>
2025-12-09 18:35:42 +0800

    Method ceil(), floor() dan round()

---
+++ Day93.java
@@ -0,0 +1,11 @@
+public class Day93 {
+    public static void main(String[] args) {
+
+        double angka = 4.7;
+
+        System.out.println("Angka asli : " + angka);
+        System.out.println("Ceil  : " + Math.ceil(angka));
+        System.out.println("Floor : " + Math.floor(angka));
+        System.out.println("Round : " + Math.round(angka));
+    }
+}
