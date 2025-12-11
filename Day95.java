fd5c95c76c77596fdd7ca941c9d4a8b3cfb4bc29
Fatmawati05 <fw841351@gmail.com>
2025-12-11 19:19:00 +0800

    Latihan: Mencetak dengan Parameter Void

---
+++ Day95.java
@@ -0,0 +1,9 @@
+public class Day95 {
+    static void cetakPesan(String pesan) {
+        System.out.println(pesan);
+    }
+    public static void main(String[] args) {
+        cetakPesan("Halo, Fatma");
+        cetakPesan("umur saya 20 tahun");
+    }
+}
