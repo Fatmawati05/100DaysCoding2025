d221741ccd52d64d064c9c16f1f1ae48dd040a03
Fatmawati05 <fw841351@gmail.com>
2025-12-12 22:19:18 +0800

    Latihan: Menghitung Penjumlahan, Pengurangan, Perkalian dan Pembagian dengan Method Non-void

---
+++ Day96.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Day96 {
+    static int tambah(int a, int b) {
+        return a + b;
+    }
+    static int kurang(int a, int b) {
+        return a - b;
+    }
+    static int kali(int a, int b) {
+        return a * b;
+    }
+    static double bagi(int a, int b) {
+        return (double) a / b;   
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan angka pertama: ");
+        int x = input.nextInt();
+
+        System.out.print("Masukkan angka kedua: ");
+        int y = input.nextInt();
+
+        System.out.println("Hasil Penjumlahan : " + tambah(x, y));
+        System.out.println("Hasil Pengurangan : " + kurang(x, y));
+        System.out.println("Hasil Perkalian   : " + kali(x, y));
+        System.out.println("Hasil Pembagian   : " + bagi(x, y)); 
+    }
+}
